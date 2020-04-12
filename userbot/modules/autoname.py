bfc102fff53c474ffb161c26482c25da6ad1eaf8
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-04-12 17:09:42 +0530

    Update autoname.py

--- userbot/modules/autoname.py
+++ userbot/modules/autoname.py
@@ -5,6 +5,7 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.autoname")
 async def update_name(name):
+  while True:
     """ For .autoname command, change your first name in Telegram and shows a running timer beside your name """
     newname = name.text[12:]
     if " " not in newname:
@@ -16,6 +17,5 @@ async def update_name(name):
         namesplit = newname.split(" ", 1)
         firstname = namesplit[0]
         lastname = namesplit[1]
-while True:
     await bot(UpdateProfileRequest(first_name=firstname, last_name=lastname))
   
