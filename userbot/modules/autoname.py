69c1d5b71cd4a380cceacd51bef0098537fdff37
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-04-12 13:47:13 +0530

    Update autoname.py

--- userbot/modules/autoname.py
+++ userbot/modules/autoname.py
@@ -1,21 +1,15 @@
 import time
 from userbot import CMD_HELP, bot
+from asyncio import sleep
 from telethon.tl.functions.account import (UpdateProfileRequest,
                                            UpdateUsernameRequest)
 from userbot.events import register
-
 @register(outgoing=True, pattern="^.autoname")
 async def update_name(name):
     """ For .autoname command, change your first name in Telegram and shows a running timer beside your name """
-    newname = name.text[20:]
-    if " " not in newname:    
-        firstname = newname
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M")
-        lastname = f"{DMY} | {HM}"
-    else:
-        namesplit = newname.split(" ", 1)
-        firstname = namesplit[0]
-        lastname = namesplit[1]
+        firstname = f"rootayush:~# ({DMY} | {HM})"
+
     while True:
-        await bot(UpdateProfileRequest(first_name=firstname, last_name=lastname))
+        await bot(UpdateProfileRequest(first_name=firstname))
