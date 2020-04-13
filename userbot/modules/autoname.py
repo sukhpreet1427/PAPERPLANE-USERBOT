fc8f80bc6f5a69dc4a2c9b86776b50c43d958962
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-04-13 09:33:26 +0530

    Update autoname.py

--- userbot/modules/autoname.py
+++ userbot/modules/autoname.py
@@ -1,4 +1,5 @@
 from asyncio import sleep
+from telethon.errors import FloodWaitError
 from time import gmtime, strftime
 from userbot import CMD_HELP, bot
 from telethon.tl.functions.account import UpdateProfileRequest
@@ -11,10 +12,14 @@ async def update_name(name):
         firstname = newname
         TIME = strftime("%Y-%m-%d %H:%M", gmtime())
         lastname = f"{TIME}"
+        TIME_OUT = 70
     else:
         namesplit = newname.split(" ", 1)
         firstname = namesplit[0]
         lastname = namesplit[1]
     while True:
+        try:
         await bot(UpdateProfileRequest(first_name=firstname, last_name=lastname))
-        time.sleep(70)
+        except FloodWaitError as ex:
+        await asyncio.sleep(ex.seconds)
+        await asyncio.sleep(TIME_OUT)
