66428a08c32cb3a3bd63dc8f2f69ba379b304da7
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-04-14 22:37:19 +0530

    Update autoname.py

--- userbot/modules/autoname.py
+++ userbot/modules/autoname.py
@@ -4,6 +4,9 @@ from telethon.errors import FloodWaitError
 from userbot import CMD_HELP, bot
 from telethon.tl.functions.account import UpdateProfileRequest
 from userbot.events import register
+from pytz import timezone
+
+tz = timezone('Asia/Kolkata')
 
 DEL_TIME_OUT = 70
 
@@ -14,7 +17,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f"⌚{HM} | ayush | 📅{DM}"
+        name = f"root@ayush:~# {DM} {HM}"
         try:
             await bot(UpdateProfileRequest(first_name=name))
         except FloodWaitError as ex:
